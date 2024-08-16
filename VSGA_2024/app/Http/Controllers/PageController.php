0c98535fc7be93094407838b14b8d1aa599d1be3
ajjee1604 <ajengprasmuuy71@gmail.com>
2024-08-16 19:19:18 +0700

    commit

---
+++ VSGA_2024/app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    public function index(){
+        return view('welcome');
+    }
+    public function satu(){
+        $arrBuah = ["Pisang", "Rambutan", "Duku", "Jambu"];
+        return view('pasarBuah')->with('pasarBuah', $arrBuah);
+    }
+}
--- VSGA_2024/routes/web.php
+++ VSGA_2024/routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\CRUDController;
+use App\Http\Controllers\CRUDPhoto;
+use App\Http\Controllers\halloController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\pengajarController;
+use App\Http\Controllers\WelcomeController;
 use illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
@@ -17,9 +23,62 @@ Route::get('/about', function(){
 Route::get('/user/{name}', function($name){
     return 'Nama Saya '.$name;
 });
-Route::get('/posts/{podt}/{comment}', function($post,$comment){
+Route::get('/posts/{post}/{comment}', function($post,$comment){
     return 'Pos ke-' . $post . "Komentar ke-:" . $comment;
 });
-Route::get('/kodebarang/{jenis}/{merek?}',function ($jk='k01',$mrk='noki'){
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya ' . $name;
+});
+Route::get('/kodebarang/{jenis?}/{merek?}', function ($jk='k01',$mrk='nokia'){
     return "kode barang $jk dan nama barang $mrk";
 });
+Route::get('about', function(){
+    return view('about');
+})->name('about');
+Route::get('tampil', function(){
+    return view('tampil');
+})->name('tampil');
+Route::get('/pesandisini', function(){
+    return '<h1> Pesan disini </h1>';
+});
+Route::redirect('/contact-us', '/pesandisini');
+Route::prefix('/polinema')->group(function(){
+Route::get('/polinema/dosen', function(){
+    return "<h1> daftar nama dosen </h1>";
+});
+Route::get('/polinema/tendik', function(){
+    return "<h1> daftar nama tendik </h1>";
+});
+Route::get('polinema/jurusan', function(){
+    return "<h1> daftar nama jurusan </h1>";
+});
+});
+Route::fallback(function(){
+    return "maaf, alamat ini tidak ditemukan";
+});
+Route::get('/daftar-dosen', [pengajarController::class,'daftarPengajar']);
+Route::get('/tabel-pengajar', [pengajarController::class,'tabelPengajar']);
+Route::get('/blog-pengajar', [pengajarController::class,'blogPengajar']);
+Route::get('pasar-buah',[PageController::class,'satu']);
+Route::resource('crud',CRUDController::class);
+Route::resource('photos', CRUDPhoto::class)->only([
+    'index',
+    'show'
+]);
+Route::resource('photos', CRUDPhoto::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
+Route::get('/selamat', function(){
+    return view('hello', ['name'=>'ajeng']);
+});
+Route::get('/greeting', [
+    WelcomeController::class,
+    'greeting'
+]);
+Route::get('Hellow',[
+    halloController::class,
+    'hellow'
+]);
